4c86ad7ab53b91c8b0f86cb3eb1df3fc9ced9d66
WebZ70 <webz70@mail.ru>
2020-03-02 18:47:10 +0600

    update setting.py

--- WebSystemsVer1/settings.py
+++ WebSystemsVer1/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -25,8 +24,7 @@ SECRET_KEY = '3@op_-pj9@g(#s$#w$tyeh!%!x16ec48ulqwajmlbdgsd7mn=='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["webz70.pythonanywhere.com"]
 
 # Application definition
 
@@ -79,18 +77,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'WebSystemsVer1.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'HOTS': '127.0.0.1',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'WebZ70$trainingsystem',
+        'USER': 'WebZ70',
+        'PASSWORD': 'piligrim1998',
+        'HOST': 'WebZ70.mysql.pythonanywhere-services.com',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -110,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -123,7 +120,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
